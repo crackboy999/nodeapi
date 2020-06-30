ad2407d38a4ec8337c1a7101cf04b87b7bafd7d6
Itsmartsapp <59887768+Itsmartsapp@users.noreply.github.com>
2020-06-30 13:40:02 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,33 @@
+
+require('./config/config');
+
+const express = require('express')
+const app = express()
+
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "GET, POST","PUT");
+  next();
+});
+ 
+app.get('/usuarios', function (req, res) {
+  res.json([{
+    "id":1,
+    "nombre":"Antonio",
+    "tipo":"Adminnistrador"
+  },{
+    "id":2,
+    "nombre":"Emanuel",
+    "tipo":"Invitado"
+  },
+  {
+    "id":3,
+    "nombre":"Moises",
+    "tipo":"Invitado"
+  }]);
+})
+ 
+app.listen(process.env.PORT,function(){
+    console.log("Server on port 4000");
+})
